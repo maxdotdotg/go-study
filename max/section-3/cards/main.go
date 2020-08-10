7c5674454f7c0123c6e2c4a414b7f6ca194c381a
Max G <maxdotdotg@gmail.com>
2020-08-10 15:58:42 -0400

    max - initial commit with notes for cards

---
+++ max/section-3/cards/main.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+var deckSize int
+
+func main() {
+    // long variable initialization form
+    //var card string = "Ace of Spades"
+
+    // short variable initialization form
+    // let the compiler figure it out
+    //card := "Ace of Spades"
+
+    // colon-equals only used when initializing a variable
+    // re-assigning a variable is only equals
+    //card = "Five of Diamonds"
+    //fmt.Println(card)
+
+    // variables can't be assigned outside of a function? 
+    // yeah, idk, makes me curious about global vars and such
+    //deckSize = 52
+    //fmt.Println(deckSize)
+
+    // use the newCard function to instantiate a
+    // you guessed it! 
+    // new card
+    //thirdCard := newCard()
+    //fmt.Println(thirdCard)
+
+
+    // let's make a slice 
+    // again, that's an array of unfixed size
+    // type declaration required
+    // a slice of type string
+    // both strings and functions that return strings are valid
+    cards := []string{"Ace of Spades", newCard()}
+
+    // let's add an item to the slice
+    // a new slice is created using the current content of `cards` plus
+    // the new item, and a new slice is returned
+    // that new since is assigned to the variable `cards`
+    // this makes me feel not great, and I am not sure why
+    // maybe it's the mutable part? IDK
+    cards = append(cards, "Six of Clubs")
+
+    // let's iterate
+    // for index and card in the slice/list/thing that is cards
+    // print the index and the data at that index
+    // the colon-equals is used because the variables i and card
+    // go out-of-scope as the loop closes
+    for i, card := range cards {
+        fmt.Println(i, card)
+    }
+    // in python:
+    // for i in cards:
+    //    print(cards)
+
+
+    // I am a bit confused by this
+    // the index returned, not the content of `cards`
+    // this is probably my defaulting to python
+    for card := range cards {
+        fmt.Println(card)
+    }
+    // the loop above does this:
+    // for i in cards:
+    //    print(cards.index(i))
+}
+
+// function signatures require return type declarations
+func newCard() string {
+    return "Five of Hearts"
+}
+
