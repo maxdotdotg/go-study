16bc4a3a63aa936b706dda78e6dd070afc5c588b
Max G <maxdotdotg@gmail.com>
2020-08-22 23:00:39 -0400

    max - embedded structs

--- max/section-4/main.go
+++ max/section-4/main.go
@@ -6,7 +6,17 @@ import "fmt"
 type person struct {
     firstName string
     lastName string
+
+    // embedded structs!
+    // makes sense, just another property/type pair
+    contact contactInfo
 }
+
+type contactInfo struct {
+    email string
+    zipCode int
+}
+
 
 func main() {
     // attribute creation/reference with dot-notation
@@ -20,12 +30,28 @@ func main() {
 
     // ZEN
     // explicit better than implicit
+    // this DOESN'T break when we add another property to the struct!
+    // I guess the contact property gets a zero value and that's it
     jenkins := person{firstName: "DEEPLOY", lastName: "JENKINS!"}
     // printf debugging
     fmt.Printf("%+v\n", jenkins)
 
+    /*
     // this looks like crap, and apparently is common
     // attributes definied with positional instead of keyword args
+    // also, this breaks when we add another property to the struct!
+    // not sure why contact doesn't get a zero value, but it doesn't? /shrug
     jimmy := person{"jimmy", "woods"}
     fmt.Println(jimmy.firstName)
+    */
+
+    bobby := person{
+        firstName: "bobby",
+        lastName: "tables",
+        contact: contactInfo{
+            email: "stuff@things.co",
+            zipCode: 11111,
+        },
+    }
+    fmt.Println(bobby.contact.email)
 }
