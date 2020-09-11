afe88da87aed13df7cfd1cef47c479383bd0bac7
Max G <maxdotdotg@gmail.com>
2020-09-10 22:21:21 -0400

    max - implement basic cat functionality

--- max/go_cat/main.go
+++ max/go_cat/main.go
@@ -2,27 +2,52 @@ package main
 
 import (
     "fmt"
+    "io"
     "os"
 )
 
-type logWriter struct{}
 
 func main(){
-    // open the file
-    file, err := os.Open(string(os.Args))
+    /* 
+    check for args!
+    there's likely a library to handle this
+    also, this was guesswork, I don't see anything in the docs
+    I'm sure it's there, but I didn't find it
+    maybe it's this?
+    https://golang.org/pkg/os/#pkg-variables
+    */
+    args := os.Args
+    if len(args) != 2 {
+        fmt.Println("accepts only one argument, a file to print to stdout")
+        os.Exit(1)
+    }
+
+    /*
+    open the file
+    https://golang.org/pkg/os/#Open
+    */
+    file, err := os.Open(string(args[1]))
     if err != nil {
         fmt.Println("error", err)
         os.Exit(1)
     }
 
-    // print the file
-    fmt.Println(*file)
+    /* 
+    I still do not understand this...
 
-    // can't use this yet, since file returns a pointer
-    //io.Copy(lw, file)
+    I THINK THIS WORKS BECAUSE
+    we're passing a pointer to io.Copy... 
+    os.Stdout implements Reader... and file implements writer?
+
+    yep, os.Open implements Write, https://golang.org/pkg/os/#File.Write
+    and io.Copy takes a pointer to something that implements Write
+
+    "Stdin, Stdout, and Stderr are open Files pointing to the standard input,
+    standard output, and standard error file descriptors."
+    https://golang.org/pkg/os/#pkg-variables
+    
+
+    */
+    io.Copy(os.Stdout, file)
 }
 
-func (logWriter) Write(bs []byte) (int, error) {
-    fmt.Println(string(bs))
-    return len(bs), nil
-}
